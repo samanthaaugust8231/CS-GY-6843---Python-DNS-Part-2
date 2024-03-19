3502ad7f480c500bc8add71ed4ea8d91afbe8ff6
samanthaaugust8231 <147224997+samanthaaugust8231@users.noreply.github.com>
2024-03-19 12:29:09 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -112,7 +112,7 @@ def run_dns_server():
             response = dns.message.make_response(request)
 
             # Get the question from the request
-            question = request.question[0]
+            question = request.question[1]
             qname = question.name.to_text()
             qtype = question.rdtype
 
