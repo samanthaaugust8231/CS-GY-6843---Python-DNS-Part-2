a189cefdfca8a019891cbdd0b799e6703d9a94b6
samanthaaugust8231 <147224997+samanthaaugust8231@users.noreply.github.com>
2024-03-19 13:12:45 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -126,7 +126,7 @@ def run_dns_server():
                 if qtype == dns.rdatatype.MX:
                     for pref, server in answer_data:
                         rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
-                elif qtype == dns.rdatatype.SOA:
+                elif qtype == dns.rdatatype.AAAA:
                     mname, rname, serial, refresh, retry, expire, minimum = answer_data # What is the record format? See dns_records dictionary. Assume we handle @, Class, TTL elsewhere. Do some research on SOA Records
                     rdata = SOA(dns.rdataclass.IN, dns.rdatatype.SOA, mname, rname, serial, refresh, retry, expire, minimum) # follow format from previous line
                     rdata_list.append(rdata)
