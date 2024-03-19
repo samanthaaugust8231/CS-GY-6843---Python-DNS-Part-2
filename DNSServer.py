5b6d8b957cb48d3a55f92497a4afb49cffb81dcc
samanthaaugust8231 <147224997+samanthaaugust8231@users.noreply.github.com>
2024-03-19 00:39:32 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -109,7 +109,7 @@ def run_dns_server():
             # Parse the request using the `dns.message.from_wire` method
             request = dns.message.from_wire(data)
             # Create a response message using the `dns.message.make_response` method
-            response = dns.message.make_response(data)
+            response = dns.message.make_response(request)
 
             # Get the question from the request
             question = request.question[0]
