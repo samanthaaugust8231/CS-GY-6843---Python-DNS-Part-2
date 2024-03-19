635b5744f374e415ad6db7bf5ffb9d3722517cdf
samanthaaugust8231 <147224997+samanthaaugust8231@users.noreply.github.com>
2024-03-19 12:36:10 -0400

    Update DNSServer.py

--- DNSServer.py
+++ DNSServer.py
@@ -123,7 +123,7 @@ def run_dns_server():
 
                 rdata_list = []
 
-                if qtype == dns.rdatatype.A:
+                if qtype == dns.rdatatype.CNAME:
                     for pref, server in answer_data:
                         rdata_list.append(MX(dns.rdataclass.IN, dns.rdatatype.MX, pref, server))
                 elif qtype == dns.rdatatype.SOA:
